bf97cae51c1406350fb9cec21df0550aff740844
Ritik Dubey <ritikdubey2664@gmail.com>
2024-08-07 19:35:35 +0000

    dataypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";// treat all js code as newer vrsion
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3+4);
+console.log("Ritik Dubey");
+
+let age=19;
+let isLoggedIn=false
+let state=null
+/* preemitive datatypes */
+//number +> 2 topower 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null => stnadalone value : null ek represntation hai empty value ka
+//undefined => abhi tk variable declared kara hai but loi value ni chaiye.(abhi tk value assign ni hui hai )
+//symbol=> uniquness find karne ke liye unique ko assign karte hai
+
+
+//object
+
+console.log(typeof "Ritik Dubey");
+console.log(typeof 13);
+console.log(typeof false);
+console.log(typeof null);// output object aata hai
+console.log(typeof undefined); //output undefined hi aata hai kyu ki o types hai js ke ander
+
+
+
+
+
