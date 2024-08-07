2d96358d79a98f8f9dde9567cb51f29e455050fb
Ritik Dubey <ritikdubey2664@gmail.com>
2024-08-07 19:02:06 +0000

    container config

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,22 @@
+const accountId=124343;
+let   accountEmail="ritik@gmail.com" ;
+var  accountPassword="1234";
+accountCity="Jaipur"
+
+let accountState;
+
+// accountId=2; //not allowed
+
+accountEmail= "virat@gmail.com";
+accountPassword="23242";
+accountCity="Bihar";
+
+console.log(accountId);
+/*
+Perfer not to be use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState]);
+
+
