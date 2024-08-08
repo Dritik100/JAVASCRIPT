68befb4354b7588de98192c4024d04450f066544
Ritik Dubey <ritikdubey2664@gmail.com>
2024-08-08 10:45:20 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score="33fgfg";
+// let score=null;
+// let score=undefined;
+// let score=true;
+let score="Ritik Dubey";
+// console.log(typeof score);
+// console.log(typeof(score))
+
+let valueInNumber= Number(score);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+//"33"=> 33
+//"33abbd" => Not a number NaN
+//true => 1; flase=> 0
+//undefined =>0
+
+// let isLoggedIn=1;
+// let isLoggedIn="";
+let isLoggedIn="Ritik Dueby";
+
+let booleanIsLoggedIn= Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//1=> true
+//0=> false
+//""=>false
+//"Ritik"=> true
+
+let someNumber=33;
+
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
