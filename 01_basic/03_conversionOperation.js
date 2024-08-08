ae1c57e9faf28c6f2e6516d386ea045c302bd002
Ritik Dubey <ritikdubey2664@gmail.com>
2024-08-08 12:23:11 +0000

    conversion and datatypes

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -30,6 +30,44 @@ let booleanIsLoggedIn= Boolean(isLoggedIn);
 let someNumber=33;
 
 let stringNumber=String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//********************Operations ***************/
+
+let value=3;
+let negValue= -value;
+// console.log(negValue);
+// console.log(2+3);
+// console.log(2-2);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1="Hello";
+let str2 = " Ritik";
+let str3=  str1  +  str2;
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log( (2+4)* (5) % 3);
+
+
+//  
+
+
+let gameCounter=100;
+// gameCounter++; postfix
+++gameCounter; //prefix
+console.log(gameCounter);
+
+
+
 
---
+++ 01_basic/04_comparssion.js
@@ -0,0 +1,29 @@
+// console.log(2 >1);
+// console.log(2 >= 1);
+// console.log(2 <1);
+// console.log(2 <=1);
+// console.log(2==1);
+// console.log(2 != 1);
+
+// console.log("2" > 0);
+// console.log("02" > 1);
+
+// console.log(null > 0); f
+// console.log(null == 0); f
+// console.log(null >= 0); t
+
+
+// console.log(undefined ==0);
+// console.log(undefined >0);
+// console.log(undefined >=0);
+// console.log(undefined <0);
+// console.log(undefined <=0);
+
+
+
+// === strict check means tripple equal check (na to ye value ko hi check ye datatype ko v check karta hai)
+
+console.log("2" === 2);
+
+
+
